4f59456696f6e14fe0a845bc6a381e077a208680
ivan-danilov <van.danilov@gmail.com>
2013-06-04 16:55:22 +0300

    Simplest MSBuild task for integrating with build in other projects. Plus targets file.

---
+++ SimpleWixDsl.MSBuild/SwixTransform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using SimpleWixDsl.Swix;
+
+namespace SimpleWixDsl.MSBuild
+{
+    public class SwixTransform : Task
+    {
+        [Required]
+        public string Source { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                Log.LogMessage(MessageImportance.Low, "Transforming {0}...", Source);
+                SwixProcessor.Transform(Source);
+            }
+            catch (Exception e)
+            {
+                Log.LogErrorFromException(e);
+                return false;
+            }
+            return true;
+        }
+    }
+}
