5ec90389d5c615acb06b1506f4fee5f35e7eaff3
ivan-danilov <van.danilov@gmail.com>
2013-06-05 14:54:09 +0300

    Added check for directories that their IDs are unique. With readable error message

--- SimpleWixDsl.Swix/WxsGeneration/WxsGenerator.cs
+++ SimpleWixDsl.Swix/WxsGeneration/WxsGenerator.cs
@@ -20,9 +20,34 @@ namespace SimpleWixDsl.Swix
             _guidProvider = guidProvider;
             int id = 1;
             foreach (var cabFile in model.CabFiles)
-            {
                 _cabFilesIds[cabFile.Name] = id++;
+
+            var nonUniqueDirectoryReadableIds = FindNonUniqueDirectoryReadableIds();
+            if (nonUniqueDirectoryReadableIds.Count > 0)
+            {
+                var msg = string.Format("The following directories' IDs are not unique: {0}. You should rename some of them or specify IDs explicitly.",
+                                        String.Join(", ", nonUniqueDirectoryReadableIds));
+                throw new SwixSemanticException(msg);
             }
+        }
+
+        private HashSet<string> FindNonUniqueDirectoryReadableIds()
+        {
+            var result = new HashSet<string>();
+            var usedIds = new HashSet<string>();
+            foreach (var dir in TraverseDfs(_model.RootDirectory, dir => dir.Subdirectories))
+            {
+                var id = GetDirectoryReadableId(dir);
+                bool isUnique = usedIds.Add(id);
+                if (!isUnique)
+                    result.Add(id);
+            }
+            return result;
+        }
+
+        private static string GetDirectoryReadableId(WixTargetDirectory dir)
+        {
+            return dir.Id ?? MakeReadableId(dir.Name);
         }
 
         public void WriteToStream(StreamWriter target)
@@ -103,8 +128,7 @@ namespace SimpleWixDsl.Swix
             foreach (var directory in rootDirectory.Subdirectories)
             {
                 doc.WriteStartElement("Directory");
-                var guid = _guidProvider.Get(SwixGuidType.Directory, directory.Name);
-                doc.WriteAttributeString("Id", directory.Id ?? MakeUniqueId(guid, directory.Name));
+                doc.WriteAttributeString("Id", GetDirectoryReadableId(directory));
                 doc.WriteAttributeString("Name", directory.Name);
                 WriteSubDirectories(doc, directory);
                 doc.WriteEndElement();
@@ -122,11 +146,32 @@ namespace SimpleWixDsl.Swix
 
         private string MakeUniqueId(Guid guid, string filename)
         {
+            var readableId = MakeReadableId(filename);
+            return String.Format("{0}_{1:N}", readableId, guid);
+        }
+
+        private static string MakeReadableId(string filename)
+        {
+            const int maxFilenameSubstringLength = 100;
+            int substringLength = Math.Min(filename.Length, maxFilenameSubstringLength);
+
+            // if filename has max acceptable length and first char is digit - we will insert
+            // underscore, thus taking one character less from filename itself
+            if (substringLength == maxFilenameSubstringLength && filename[0] >= '0' && filename[0] <= '9')
+                substringLength--;
+
+            // <optional-underscore><filename-substring><underscore><guid> == 1 + substringLength + 1 + 32
+            var sb = new StringBuilder(filename, 0, substringLength, substringLength + 34);
+            CleanseForUseAsId(sb);
+
             if (filename[0] >= '0' && filename[0] <= '9')
-                filename = "_" + filename;
-            if (filename.Length > 100)
-                filename = filename.Substring(0, 100);
-            var sb = new StringBuilder(filename, filename.Length + 33);
+                sb.Insert(0, '_');
+
+            return sb.ToString();
+        }
+
+        private static void CleanseForUseAsId(StringBuilder sb)
+        {
             for (int i = 0; i < sb.Length; i++)
             {
                 bool allowed = sb[i] >= 'a' && sb[i] <= 'z' ||
@@ -136,8 +181,16 @@ namespace SimpleWixDsl.Swix
                 if (!allowed)
                     sb[i] = '_';
             }
-            sb.AppendFormat("_{0:N}", guid);
-            return sb.ToString();
+        }
+
+        private static IEnumerable<T> TraverseDfs<T>(T root, Func<T, IEnumerable<T>> childrenSelector)
+        {
+            foreach (var child in childrenSelector(root))
+            {
+                yield return child;
+                foreach (var subtreeNode in TraverseDfs(child, childrenSelector))
+                    yield return subtreeNode;
+            }
         }
     }
 }
