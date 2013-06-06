e8e1d674289b40cb1c5f2078c8d3d852bb51b70a
ivan-danilov <van.danilov@gmail.com>
2013-06-06 14:16:55 +0300

    Modified GuidProvider so that old guids are not re-saved if weren't used in current session

--- SimpleWixDsl.Swix/WxsGeneration/GuidProvider.cs
+++ SimpleWixDsl.Swix/WxsGeneration/GuidProvider.cs
@@ -20,12 +20,16 @@ namespace SimpleWixDsl.Swix
                 var match = LineRegex.Match(line);
                 var path = match.Groups["path"].Value;
                 var guid = Guid.Parse(match.Groups["guid"].Value);
-                result._map.Add(path, guid);
+                result._loadedFromFileGuids.Add(path, guid);
             }
             return result;
         }
 
-        private readonly Dictionary<string, Guid> _map = new Dictionary<string, Guid>();
+        // Loaded from file are just for reference, current ones will be re-saved.
+        // Values are moved from loaded to current when used.
+        // Thus old values that were not used in current session will not be re-saved again.
+        private readonly Dictionary<string, Guid> _loadedFromFileGuids = new Dictionary<string, Guid>();
+        private readonly Dictionary<string, Guid> _currentGuids = new Dictionary<string, Guid>();
 
         public Guid Get(SwixGuidType type, string path)
         {
@@ -34,17 +38,22 @@ namespace SimpleWixDsl.Swix
             var key = String.Format("{0}\\\"{1}\"", type, path);
 
             Guid result;
-            if (!_map.TryGetValue(key, out result))
+            if (!_currentGuids.TryGetValue(key, out result))
             {
+                if (_loadedFromFileGuids.TryGetValue(key, out result))
+                {
+                    _currentGuids[key] = result;
+                    return result;
+                }
                 result = Guid.NewGuid();
-                _map[key] = result;
+                _currentGuids[key] = result;
             }
             return result;
         }
 
         public void SaveToStream(StreamWriter stream)
         {
-            foreach (var pair in _map.OrderBy(pair => pair.Key))
+            foreach (var pair in _currentGuids.OrderBy(pair => pair.Key))
             {
                 var key = pair.Key;
                 var guid = pair.Value;
