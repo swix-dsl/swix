8166e8787f40d76fe17f86114f3ad0426f132690
ivan-danilov <van.danilov@gmail.com>
2013-09-05 17:32:24 +0300

    Added exceptPathRegex argument to ?harvest meta in the :components section

--- SimpleWixDsl.Swix/SemanticContexts/ComponentsSection.cs
+++ SimpleWixDsl.Swix/SemanticContexts/ComponentsSection.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using SimpleWixDsl.Ahl;
 
 namespace SimpleWixDsl.Swix
@@ -36,41 +38,62 @@ namespace SimpleWixDsl.Swix
         public ISemanticContext HandleMetaHarvest(string key, IAttributeContext metaContext)
         {
             return new StubSwixElement(null, () =>
-                {
-                    if (!Directory.Exists(key))
-                        throw new SwixSemanticException(FormatError("Directory '" + key + "' does not exists"));
-                    var attrs = metaContext.GetDirectlySetAttributes();
-                    string filter;
-                    if (!attrs.TryGetValue("filter", out filter))
-                        filter = "*.*";
-                    bool withSubfolders = attrs.ContainsKey("withSubfolders") && attrs["withSubfolders"] == "yes";
+            {
+                if (!Directory.Exists(key))
+                    throw new SwixSemanticException(FormatError("Directory '" + key + "' does not exists"));
+                var attrs = metaContext.GetDirectlySetAttributes();
+                string filter;
+                if (!attrs.TryGetValue("filter", out filter))
+                    filter = "*.*";
 
-                    key = Path.GetFullPath(key); // eliminates relative parts like a\b\..\b2\c
-                    var files = Directory.GetFiles(key, filter, withSubfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
-                    foreach (var filepath in files)
+                Regex excludePathRegex;
+                string excludePathRegexStr;
+                if (!attrs.TryGetValue("excludePathRegex", out excludePathRegexStr))
+                {
+                    excludePathRegex = new Regex("a^", RegexOptions.Compiled); // will match nothing
+                }
+                else
+                {
+                    try
                     {
-                        int harvestingRootSubstringLength = key.Length;
-                        if (filepath[harvestingRootSubstringLength] == '\\')
-                            harvestingRootSubstringLength++;
-                        var relativeToHarvestingRoot = filepath.Substring(harvestingRootSubstringLength);
-                        var relativeDir = Path.GetDirectoryName(relativeToHarvestingRoot);
-                        try
-                        {
-                            var component = WixComponent.FromContext(filepath, CurrentAttributeContext);
-                            if (relativeDir != null)
-                            {
-                                component.TargetDir = component.TargetDir == null
-                                                          ? relativeDir
-                                                          : Path.Combine(component.TargetDir, relativeDir);
-                            }
-                            _toAdd.Add(component);
-                        }
-                        catch (SwixSemanticException e)
-                        {
-                            throw new SwixSemanticException(FormatError("During harvesting of {0} file, exception occurs:\n{1}", filepath, e.ToString()));
-                        }
+                        excludePathRegex = new Regex(excludePathRegexStr, RegexOptions.Compiled);
                     }
-                });
+                    catch (ArgumentException e)
+                    {
+                        throw new SwixSemanticException(FormatError("Invalid Regex pattern in excludePathRegex argument: {0}", e.ToString()));
+                    }
+                }
+
+                bool withSubfolders = attrs.ContainsKey("withSubfolders") && attrs["withSubfolders"] == "yes";
+
+                key = Path.GetFullPath(key); // eliminates relative parts like a\b\..\b2\c
+                var files = Directory.GetFiles(key, filter, withSubfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+                foreach (var filepath in files)
+                {
+                    int harvestingRootSubstringLength = key.Length;
+                    if (filepath[harvestingRootSubstringLength] == '\\')
+                        harvestingRootSubstringLength++;
+                    var relativeToHarvestingRoot = filepath.Substring(harvestingRootSubstringLength);
+                    if (excludePathRegex.IsMatch(relativeToHarvestingRoot))
+                        continue;
+                    var relativeDir = Path.GetDirectoryName(relativeToHarvestingRoot);
+                    try
+                    {
+                        var component = WixComponent.FromContext(filepath, CurrentAttributeContext);
+                        if (relativeDir != null)
+                        {
+                            component.TargetDir = component.TargetDir == null
+                                ? relativeDir
+                                : Path.Combine(component.TargetDir, relativeDir);
+                        }
+                        _toAdd.Add(component);
+                    }
+                    catch (SwixSemanticException e)
+                    {
+                        throw new SwixSemanticException(FormatError("During harvesting of {0} file, exception occurs:\n{1}", filepath, e.ToString()));
+                    }
+                }
+            });
         }
 
         protected override void FinishItemCore()
