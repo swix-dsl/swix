e2f483166085488ea0bac9e9858f742727bc35be
ivan-danilov <van.danilov@gmail.com>
2020-01-09 19:51:42 -0800

    Command line app now is able to receive swix variables

--- SimpleWixDsl.SwixTransform/Program.cs
+++ SimpleWixDsl.SwixTransform/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SimpleWixDsl.Swix;
 
 namespace SimpleWixDsl.SwixTransform
@@ -13,7 +10,31 @@ namespace SimpleWixDsl.SwixTransform
         private static void Main(string[] args)
         {
             var targetPath = Path.GetDirectoryName(args[0]);
-            SwixProcessor.Transform(args[0], SwixGuidMode.UseExistingAndUpdateStorage, targetPath);
+            var swixVars = args.Length == 1 ? null : GetArgs(args[1]);
+            if (swixVars != null)
+            {
+                Console.WriteLine("Arguments found:");
+                foreach (var variable in swixVars)
+                    Console.WriteLine($"{variable.Key}={variable.Value}");
+            }
+
+            SwixProcessor.Transform(args[0], SwixGuidMode.UseExistingAndUpdateStorage, targetPath, swixVars);
+        }
+
+        private static Dictionary<string, string> GetArgs(string source)
+        {
+            var res = new Dictionary<string, string>();
+            var split = source.Split(new[] {";"}, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var arg in split)
+            {
+                var eqIdx = arg.IndexOf('=', 0);
+                if (eqIdx == -1) continue;
+                var name = arg.Substring(0, eqIdx);
+                var val = arg.Substring(eqIdx + 1);
+                res[name] = val;
+            }
+
+            return res;
         }
     }
 }
